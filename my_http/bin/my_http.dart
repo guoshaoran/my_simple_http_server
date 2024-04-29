f6ef1df2665233f8d128f793e698000045bfde76
SweetPresent <142983461+guoshaoran@users.noreply.github.com>
2024-04-29 15:54:01 +0000

    11

---
+++ my_http/bin/my_http.dart
@@ -0,0 +1,5 @@
+import 'package:my_http/my_http.dart' as my_http;
+
+void main(List<String> arguments) {
+  print('Hello world: ${my_http.calculate()}!');
+}
---
+++ my_http/test/my_http_test.dart
@@ -0,0 +1,8 @@
+import 'package:my_http/my_http.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
